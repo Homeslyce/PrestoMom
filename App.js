95a8fbc88ddeafa783f3f992affacdc96b9b1aad
unknown <alexandre.bruneau.1@etsmtl.net>
2021-01-17 17:33:18 -0500

    Added React navigation to the project

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler"; //must be at the top
 import React from "react";
-import { View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import MainLayout from "./components/mainLayout";
+//import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./components/home/homeScreen";
+import RecipeScreen from "./components/recipe/recipeScreen";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+    const Stack = createStackNavigator();
+
     return (
-        <PaperProvider>
-            <View>
-                <MainLayout />
-                <StatusBar style="auto" />
-            </View>
-        </PaperProvider>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Welcome" }} />
+                <Stack.Screen name="Recipe" component={RecipeScreen} />
+            </Stack.Navigator>
+            <StatusBar style="auto" />
+        </NavigationContainer>
     );
 }
