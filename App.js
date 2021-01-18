f65dd8e1a3f39bd4a5227d4dedc7aed54e52447f
unknown <alexandre.bruneau.1@etsmtl.net>
2021-01-17 19:42:14 -0500

    Added headers and main logo

--- App.js
+++ App.js
@@ -1,22 +1,63 @@
 import "react-native-gesture-handler"; //must be at the top
 import React from "react";
-//import { StyleSheet } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./components/home/homeScreen";
 import RecipeScreen from "./components/recipe/recipeScreen";
 import { StatusBar } from "expo-status-bar";
+import { colors } from "./styles/base";
+import mainLogo from "./assets/mainLogo.png";
 
 export default function App() {
     const Stack = createStackNavigator();
 
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Welcome" }} />
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: colors.primary,
+                    },
+                    headerTintColor: "#fff",
+                    headerTitleStyle: {
+                        fontWeight: "bold",
+                    },
+                }}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        headerTitle: function homepageHeader() {
+                            return <Image style={styles.headerLogo} source={mainLogo} />;
+                        },
+                    }}
+                />
                 <Stack.Screen name="Recipe" component={RecipeScreen} />
             </Stack.Navigator>
             <StatusBar style="auto" />
         </NavigationContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    mainContrainer: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    mainHeader: {
+        backgroundColor: colors.primary,
+        paddingTop: 50,
+        height: 115,
+    },
+    headerLogo: {
+        width: 200,
+        height: 50,
+        alignSelf: "center",
+    },
+    receipe: {
+        width: 150,
+    },
+});
